8b738ce0f0a6072ef1187283f08547496de50967
KateM3d <ekaterina8medvedeva@gmail.com>
2024-01-05 16:22:44 -0500

    add bcrypt for password and user creation

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User.js");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(
   cors({
@@ -9,10 +16,27 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  });
+
+  res.json(newUser);
+});
+
 const PORT = 4000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
